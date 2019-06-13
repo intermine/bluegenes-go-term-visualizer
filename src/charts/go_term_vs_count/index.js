6b658a47eaf98ad2747ec4e7c9b098dcf94efe33
akshat <akshatbhargavadeepa@gmail.com>
2019-06-13 21:20:51 +0530

    added GoTerm_vs_Count chart

---
+++ src/charts/go_term_vs_count/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Chart from 'chart.js';
+import getChartData from './chartData';
+
+class GoTerm_vs_Count extends React.Component {
+	constructor(props) {
+		super(props);
+		this.chart = React.createRef();
+	}
+
+	componentDidUpdate() {
+		const { data } = this.props;
+		if (!data) return;
+
+		const chartData = getChartData(data);
+
+		new Chart(this.chart.current, {
+			type: 'bar',
+			data: {
+				labels: chartData.labels,
+				datasets: chartData.datasets
+			},
+			options: chartData.options
+		});
+	}
+
+	render() {
+		const { data } = this.props;
+		if (!data) return 'Loading';
+
+		return (
+			<div>
+				<canvas ref={this.chart} />
+			</div>
+		);
+	}
+}
+
+export default GoTerm_vs_Count;
