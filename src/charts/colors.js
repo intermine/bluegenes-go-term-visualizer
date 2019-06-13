f11bd055f61ef36a8ba509e11368e5ca1cd5814d
akshat <akshatbhargavadeepa@gmail.com>
2019-06-13 11:05:08 +0530

    added const shared colors file

---
+++ src/charts/colors.js
@@ -0,0 +1,12 @@
+export const getColors = (alpha = 1) => [
+	`rgb(57, 119, 175, ${alpha})`,
+	`rgb(239, 133, 54, ${alpha})`,
+	`rgb(81, 157, 62, ${alpha})`,
+	`rgb(197, 57, 50, ${alpha})`,
+	`rgb(141, 107, 184, ${alpha})`,
+	`rgb(133, 88, 77, ${alpha})`,
+	`rgb(213, 126, 190, ${alpha})`,
+	`rgb(127, 127, 127, ${alpha})`,
+	`rgb(189, 188, 69, ${alpha})`,
+	`rgb(87, 188, 204, ${alpha})`
+];
