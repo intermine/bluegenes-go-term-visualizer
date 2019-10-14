bb72d5d24eeb2675cc7e448c610930ee2ccaac90
Akshat <akshatbhargavadeepa@gmail.com>
2019-10-14 22:29:16 +0530

    Fixed the test removal blunt!

--- tests/charts.test.js
+++ tests/charts.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import imjs from 'imjs';
-import GoTerm_vs_P from '../src/charts/go_term_vs_p';
+import GoTerm_vs_Count from '../src/charts/go_term_vs_count';
 import queryData from '../src/query';
 
 describe('charts', () => {
@@ -14,9 +14,9 @@ describe('charts', () => {
 		).then(res => (data = res));
 	});
 
-	test('go_term_vs_p renders canvas', () => {
-		const el = document.createElement('div');
-		ReactDOM.render(<GoTerm_vs_P data={data} />, el);
-		expect(el.innerHTML).toContain('canvas');
+	test('go_term_vs_count renders canvas', () => {	
+		const el = document.createElement('div');	
+		ReactDOM.render(<GoTerm_vs_Count data={data} />, el);	
+		expect(el.innerHTML).toContain('canvas');	
 	});
 });
