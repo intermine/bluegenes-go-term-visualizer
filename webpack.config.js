e3140a9ce3297dd345dd4d03e296555b4b4f7240
uosl <regen@anche.no>
2020-06-23 16:56:05 +0100

    Use more conventional library var and remove unneeded deps
    The deps were included as a workaround for tests prior to imjs 4.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    library: "bluegenes_GO_term_visualizer",
+    library: "bluegenesGoTermVisualizer",
     libraryTarget: "var"
   },
   optimization: {
